e538c47a9c5b6ea2af98958f2fb54cabc92fd4e2
ditikrishnabiswal <ditikrishnabiswal@gmail.com>
2026-02-22 23:13:57 +0530

    program7,8,9

---
+++ pythonniis/program7,8,9/squ.py
@@ -0,0 +1,7 @@
+8.#square and cub
+print("enter number")
+n=int(input())
+sq=n**2
+cb=n**3
+print("square=",sq)
+print("cube=",cb)
---
+++ pythonniis/program7,8,9/sur.py
@@ -0,0 +1,5 @@
+7.# surface area of sphere
+print("enter radius")
+r=float(input())
+area=4*3.14*r*r
+print("surface area=",area)
